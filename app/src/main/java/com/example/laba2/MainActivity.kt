db29a2ce396ff60473848e067a8a29f074563e7f
hai-mark <errrorfour@gmail.com>
2024-10-10 01:56:50 +0700

    Добавление методов

--- app/src/main/java/com/example/laba2/MainActivity.kt
+++ app/src/main/java/com/example/laba2/MainActivity.kt
@@ -6,7 +6,17 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var inputN: EditText
+    private lateinit var inputA: EditText
+    private lateinit var calculateButton: Button
+    private lateinit var result: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +26,41 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        inputN = findViewById(R.id.inputN)
+        inputA = findViewById(R.id.inputA)
+        calculateButton = findViewById(R.id.calculateButton)
+        result = findViewById(R.id.result)
+
+        calculateButton.setOnClickListener { calculate() }
+    }
+
+    private fun calculate() {
+        val aString = inputA.text.toString()
+        val nString = inputN.text.toString()
+
+        if (aString.isNotEmpty() && nString.isNotEmpty()) {
+            val a = aString.toDouble()
+            val n = nString.toInt()
+
+            if (a != 0.0) { // Проверка на деление на ноль
+                val resultvar = SumResult(a, n)
+                result.text = "Результат: $resultvar"
+            } else {
+                result.text = "Значение a не должно быть равно 0."
+            }
+        } else {
+            result.text = "Введите значения a и n."
+        }
+    }
+
+    private fun SumResult(a: Double, n: Int): Double {
+        var sum = 0.0
+
+        for (i in 0..n) {
+            sum += 1 / (a * (a + i))
+        }
+
+        return sum
     }
 }
